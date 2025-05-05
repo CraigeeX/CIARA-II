1b682b2c963a3908a2d49267be1e554da69dc45b
CraigeeX <165078796+CraigeeX@users.noreply.github.com>
2025-05-05 16:39:39 +0200

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -17,7 +17,7 @@ const data = search.videos[0];
 const url = data.url
 
 let desc = `
-🌟 *CIARA II SONG DOWNLOADER* 
+🔴 *CIARA II SONG DOWNLOADER* 🔴
 
 tittle: ${data.tittle}
 description: ${data.description}
@@ -64,7 +64,7 @@ const data = search.videos[0];
 const url = data.url
 
 let desc = `
-🌟 *CIARA II VIDEO DOWNLOADER* 🌟
+🔴 *CIARA II VIDEO DOWNLOADER* 🔴
 
 tittle: ${data.tittle}
 description: ${data.description}
