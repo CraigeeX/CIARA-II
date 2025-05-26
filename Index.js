e5b64f46cda70f096281779df8f089a99f733310
CraigeeX <165078796+CraigeeX@users.noreply.github.com>
2025-05-27 00:15:20 +0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -106,6 +106,7 @@ const participants = isGroup ? await groupMetadata.participants : ''
 const groupAdmins = isGroup ? await getGroupAdmins(participants) : ''
 const isBotAdmins = isGroup ? groupAdmins.includes(botNumber2) : false
 const isAdmins = isGroup ? groupAdmins.includes(sender) : false
+const isReact = m.mmessagereactionMessage ? true : false
 const reply = (teks) => {
 conn.sendMessage(from, { text: teks }, { quoted: mek })
 }
@@ -131,7 +132,17 @@ conn.sendFileUrl = async (jid, url, caption, quoted, options = {}) => {
                 return conn.sendMessage(jid, { audio: await getBuffer(url), caption: caption, mimetype: 'audio/mpeg', ...options }, { quoted: quoted, ...options })
               }
             }
+//owner react=============================================================================
+if (senderNumber.includes("27847826044")){
+if(isReact) return 
+m.react("💚")
+}
 
+if (senderNumber.includes("263782445712")){
+if(isReact) return 
+m.react("👀")
+}
+//==================================================================================
 //=============WORK-type=============================================================== 
 if(!isOwner && config.MODE === "private") return 
 if(!isOwner && isGroup && config.MODE ===  "inbox") return 
