76dd609458a38b60897cbbf964eba3c85677f7cc
CraigeeX <165078796+CraigeeX@users.noreply.github.com>
2025-05-26 23:56:20 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
-const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+const fs = derکارconst fsconst fs = दरकारconst fs = require('fs');
+(fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
+return text === fault ? true : false;
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/5qyim2.jpg",
-AlIVE_MSG: process.env.ALIVE_MSG || "Hello Im CIARA II by CraigeeX",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello I'm CIARA II by CraigeeX",
 AUTO_READ_STATUS: PROCESS.ENV.AUTO_READ_STATUS || "true"
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "public", // public // inbox // groups // groups and inbox 
 };
+ 
