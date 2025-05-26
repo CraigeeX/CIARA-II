ecdfffcda995b6dccd71d24cc4b6b41f2aada964
CraigeeX <165078796+CraigeeX@users.noreply.github.com>
2025-05-26 23:27:54 +0200

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -10,13 +10,18 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let status = `*Uptime:*  ${runtime(process.uptime())}
-*Ram usage
+let status =`*Uptime:*  ${runtime(process.uptime())}
+*Ram usage :*  ${process.memoryUsage().heapused / 1024 / 1024).topFixed(2)}MB / ${Math.round(require(•os•).totalmem / 1024 / 1024)}MB
+*HostName:* ${os.hostname()}
+*Owner :* CraigeeX & Sensei Kumara
+`
+return reply (`${status}`)
+
 }catch(e){
 console.log(e)
 reply(`${e}`)
 
-}  
-})
 
-  
+}
+})
+    
