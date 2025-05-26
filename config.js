2d3acb4264fd942b9fb2c1f144c3b8f86db13f70
CraigeeX <165078796+CraigeeX@users.noreply.github.com>
2025-05-26 23:36:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,5 @@ SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/5qyim2.jpg",
 AlIVE_MSG: process.env.ALIVE_MSG || "Hello Im CIARA II by CraigeeX",
 AUTO_READ_STATUS: PROCESS.ENV.AUTO_READ_STATUS || "true"
+MODE: process.env.MODE || "public",
 };
