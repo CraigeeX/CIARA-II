727ea5f177cb9a87c52a9164ebeb57d6fb4f17ab
CraigeeX <165078796+CraigeeX@users.noreply.github.com>
2025-06-03 21:51:05 +0200

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,4 +1,4 @@
-const config = require('../config')
+const {readEnv} = require('../lib/database')
 const {cmd , commands} = require('../command')
 
 cmd({
@@ -9,6 +9,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+const config = await reachEnv();
 let menu = {
 main: '',
 download: '',
@@ -20,14 +21,13 @@ search: ''
   
 for (let i = 0; i < commands.length; i++) {
 if (command[i].pattern && !commands[i].dontAddCommandList) {
-menu[commands[i].category] += `.${commands[i].pattern}\n`;
+menu[commands[i].category] += `.${config. PREFIX}${commands[i].pattern}\n`;
  }
 }                              
 
 let madeMenu = `🥀 *Hello ${pushname}*`
 > *DOWNLOAD COMMANDS* ⏬️
 ${menu.download}
-
 
 > *MAIN COMMANDS* 🔰
 
@@ -51,8 +51,7 @@ ${menu.search}
     
 GANGSTER MADE IT 🇿🇼
 `
-await conn.sendMessage(from,{image:{url:"https://files.catbox.moe/5qyim2.jpg"},caption:madeMenu},{quoted:mek})
-
+await conn.sendMessage(from,{image:{url:config.ALIVE_MSG},caption:madeMenu},{quoted:mek})
 
 
 }catch(e){
