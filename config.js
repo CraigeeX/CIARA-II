b1e3ef44110ee7467bc26e0473329199e9e6302c
CraigeeX <165078796+CraigeeX@users.noreply.github.com>
2025-06-03 16:21:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,6 @@ return text === fault ? true : false;
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/5qyim2.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "Hello I'm CIARA II by CraigeeX",
-AUTO_READ_STATUS: PROCESS.ENV.AUTO_READ_STATUS || "true"
-MODE: process.env.MODE || "public", // public // inbox // groups // groups and inbox 
+MONGODB: process.env.MONGODB || "enter mongodb here" 
 };
  
